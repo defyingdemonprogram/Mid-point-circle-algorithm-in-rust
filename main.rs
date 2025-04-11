ef4b031e1fb2f78909e05b45f3522746e8f1cee5
realsanjeev2 <realsanjeev2@gmail.com>
2025-04-11 16:58:59 +0545

    Used buffer to write in file in save_as_ppm

--- main.rs
+++ main.rs
@@ -3,18 +3,18 @@ use std::io;
 use std::io::Write;
 
 fn save_as_ppm(file_path: &str, pixels: &[u32], width: usize, height: usize) -> io::Result<()> {
-    let mut file = File::create(file_path)?;
-    write!(file, "P6\n{} {} 255\n", width, height)?;
+    let mut buffer = Vec::<u8>::new();
     for y in 0..height {
         for x in 0..width {
             let pixel = pixels[y * width + x];
-            let color = [((pixel >> 8 * 2) & 0xFF) as u8, // Red
-                         ((pixel >> 8 * 1) & 0xFF) as u8, // Green
-                         ((pixel >> 8 * 0) & 0xFF) as u8  // Blue
-                        ];
-            file.write_all(&color)?;
+            buffer.push(((pixel >> 8 * 2) & 0xFF) as u8);
+            buffer.push(((pixel >> 8 * 1) & 0xFF) as u8);
+            buffer.push(((pixel >> 8 * 0) & 0xFF) as u8);
         }
     }
+    let mut file = File::create(file_path)?;
+    write!(file, "P6\n{} {} 255\n", width, height)?;
+    file.write(&buffer)?;
     println!("The file was saved at {}!", file_path);
     Ok(())
 }
